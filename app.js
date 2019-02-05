b28b031c0aa72f39f7f3e1ac6edf91c0e15b624c
shahshahid81 <shahshahid81@gmail.com>
2019-02-05 18:59:02 +0530

    Added ejs

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.set('view engine','ejs');
+
+// app.get('/',function(req,res){
+// 	res.sendFile(path.join(__dirname,'public/index.html'));
+// });
+
+// app.get('/cart',function(req,res){
+// 	res.sendFile(path.join(__dirname,'public/cart.html'));
+// });
+
+// app.get('/order',function(req,res){
+// 	res.sendFile(path.join(__dirname,'public/order.html'));
+// });
 
 app.get('/',function(req,res){
-	res.sendFile(path.join(__dirname,'public/index.html'));
+	res.render('index');
 });
 
-app.get('/cart',function(req,res){
-	res.sendFile(path.join(__dirname,'public/cart.html'));
+app.get('/menu',function(req,res){
+	res.render('menu');
 });
 
-app.get('/order',function(req,res){
-	res.sendFile(path.join(__dirname,'public/order.html'));
-});
-
-app.listen(3000,function(){
-	console.log('Server started');
+app.listen(4000,function(){
+	console.log('Server Started.....');
 });
