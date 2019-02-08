b11a70d4a504bc599aafd7fe5d951ec52e78780f
shahshahid81 <shahshahid81@gmail.com>
2019-02-09 00:48:52 +0530

    linked db with menu page

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+const Food = require("./models/food");
 
 const app = express();
 
+mongoose.connect("mongodb://127.0.0.1:27017/foodDB",{ useNewUrlParser:true },function(err){
+	if(err){
+		console.log(err);
+	}
+	else{
+		console.log('connected');
+	}
+});
+
 app.use(express.static(__dirname + "/public"));
 app.set('view engine','ejs');
-
-// app.get('/',function(req,res){
-// 	res.sendFile(path.join(__dirname,'public/index.html'));
-// });
-
-// app.get('/cart',function(req,res){
-// 	res.sendFile(path.join(__dirname,'public/cart.html'));
-// });
-
-// app.get('/order',function(req,res){
-// 	res.sendFile(path.join(__dirname,'public/order.html'));
-// });
 
 app.get('/',function(req,res){
 	res.render('index');
 });
 
 app.get('/menu',function(req,res){
-	res.render('menu');
+	Food.find({Category:"Chicken"},function(err,allFoods){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("menu",{Foods:allFoods});
+		}
+	});
 });
 
-app.listen(4000,function(){
-	console.log('Server Started.....');
+app.get('*',function(req,res){
+	res.send("page not found");
+});
+
+app.listen(3000,function(err){
+	if(err){
+		console.log(err);
+	}else{
+		console.log('Server Started.....');
+	}
 });
---
+++ models/food.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const foodSchema = mongoose.Schema({
+    Name : {
+        type : String,
+        required : true,
+        minlength : 1,
+        trim : true
+    },
+    Description : {
+        type : String,
+        required : true,
+        minlength : 1,
+        trim : true
+    },
+    Price : {
+        type : Number,
+        required : true,
+    },
+    Category : {
+        type : String,
+        required : true,
+        minlength : 1,
+        trim : true
+    },
+    ImagePath : {
+        type : String,
+        required : true
+    }
+});
+
+module.exports =  mongoose.model("Food",foodSchema,'food');
+
